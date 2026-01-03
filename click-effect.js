acf5b3f2bef0931427a6f05a43a2fcc9a0a894c7
Freesia0204 <dy1090128@gmail.com>
2026-01-04 02:04:25 +0800

    更新於 2026-01-04 02:04

--- click-effect.js
+++ click-effect.js
@@ -1,6 +1,37 @@
 // 定義所有特效主題
 const clickThemes = {
-  柯南: ['img-chick/柯1.png', 'img-chick/柯2.png','img-chick/柯1.png'],
+  柯南: [
+  'img-chick/柯1.png',
+  'img-chick/柯2.png',
+  'img-chick/柯3.png',
+  'img-chick/柯4.png',
+  'img-chick/柯5.png',
+  'img-chick/柯6.png',
+  'img-chick/柯7.png',
+  'img-chick/柯8.png',
+  'img-chick/柯9.png',
+  'img-chick/柯10.png',
+  'img-chick/柯11.png',
+  'img-chick/柯12.png',
+  'img-chick/柯13.png',
+  'img-chick/柯14.png',
+  'img-chick/柯15.png',
+  'img-chick/柯16.png',
+  'img-chick/柯17.png',
+  'img-chick/柯18.png',
+  'img-chick/柯19.png',
+  'img-chick/柯20.png',
+  'img-chick/柯21.png',
+  'img-chick/柯22.png',
+  'img-chick/柯23.png',
+  'img-chick/柯24.png',
+  'img-chick/柯25.png',
+  'img-chick/柯26.png',
+  'img-chick/柯27.png',
+  'img-chick/柯28.png',
+  'img-chick/柯29.png'
+],
+
   Free: ['img/effects/free.png', 'img/effects/makoto.png']
 };
 
